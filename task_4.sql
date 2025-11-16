a89d11286babd950d104d51f974c944bd08a88ce
Precious <preciousmofeoluwa@gmail.com>
2025-11-16 23:23:39 +0100

    f

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,17 @@
 -- Task 4: Print full description of the books table
--- Current Date and Time (UTC): 2025-11-16 22:17:46
+-- Current Date and Time (UTC): 2025-11-16 22:22:57
 -- Current User's Login: AirdropGucci
--- Note: Not using DESCRIBE or EXPLAIN statements
+-- Note: Not using DESCRIBE or EXPLAIN or ANALYZE statements
+
+USE alx_book_store;
 
 SELECT 
-  COLUMN_NAME,
-  COLUMN_TYPE,
-  IS_NULLABLE,
-  COLUMN_KEY,
-  COLUMN_DEFAULT,
-  EXTRA
+  COLUMN_NAME AS 'Field',
+  COLUMN_TYPE AS 'Type',
+  IS_NULLABLE AS 'Null',
+  COLUMN_KEY AS 'Key',
+  COLUMN_DEFAULT AS 'Default',
+  EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
 ORDER BY ORDINAL_POSITION;
