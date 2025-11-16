90df72c8c00f2e112b709a95ccaa2c47e3c53bde
Precious <preciousmofeoluwa@gmail.com>
2025-11-16 23:21:00 +0100

    s

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,7 @@
 -- Task 3: List all tables in alx_book_store database
--- Current Date and Time (UTC): 2025-11-16 22:17:46
+-- Current Date and Time (UTC): 2025-11-16 22:20:33
 -- Current User's Login: AirdropGucci
+
+USE alx_book_store;
 
 SHOW TABLES;
