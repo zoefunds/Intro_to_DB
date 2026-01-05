b0c45ab2b48829be924844ca97fdea8fefa3db99
Precious <preciousmofeoluwa@gmail.com>
2026-01-05 12:43:09 +0100

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,11 @@
--- Task 4: Print full description of the books table
--- Current Date and Time (UTC): 2025-11-16 22:22:57
--- Current User's Login: AirdropGucci
--- Note: Not using DESCRIBE or EXPLAIN or ANALYZE statements
-
-USE alx_book_store;
-
 SELECT 
-  COLUMN_NAME AS 'Field',
-  COLUMN_TYPE AS 'Type',
-  IS_NULLABLE AS 'Null',
-  COLUMN_KEY AS 'Key',
-  COLUMN_DEFAULT AS 'Default',
-  EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM INFORMATION_SCHEMA. COLUMNS 
+WHERE TABLE_SCHEMA = DATABASE() 
+AND TABLE_NAME = 'books' 
 ORDER BY ORDINAL_POSITION;
--- task_5.sql
+++
@@ -1,6 +0,0 @@
--- Task 5: Insert a single row in the customers table
--- Current Date and Time (UTC): 2025-11-16 22:17:46
--- Current User's Login: AirdropGucci
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
